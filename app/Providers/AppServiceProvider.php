6f432623b83ead8fc6176ab3adbba2dc1cb28f9d
ksroor <ksroor@pyramidsfreight.com>
2022-10-23 14:38:06 +0200

    intgrate laravel with phpstorm

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+      if ($this->app->isLocal()) {
+        $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+      }
+
         //
     }
 
